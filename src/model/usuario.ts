5ae058c590ee849fb26dd25b8ce0e24ee931d9a9
Luiz Gustavo <lgml85@hotmail.com>
2018-01-22 23:35:56 -0200

    Cadastro de usuário

---
+++ src/model/usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+    constructor(
+        public codigo: number,
+        public email: string,
+        public senha: string,
+        public dataAceiteTermoUso: string) {
+
+        }
+}
---
+++ src/pages/registro/registro.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
+import { AuthenticationProvider } from '../../providers/authentication/authentication';
+import { Storage } from "@ionic/storage";
+import { ToastController } from 'ionic-angular';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Usuario } from '../../model/usuario';
+
+/**
+ * Generated class for the RegistroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-registro',
+  templateUrl: 'registro.html',
+})
+export class RegistroPage {
+  usuario : Usuario;
+  confirmarSenha : String;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public menuCtrl: MenuController,
+    private authProvider: AuthenticationProvider,
+    private storage: Storage,
+    private toastCtrl: ToastController
+    ) {
+      this.confirmarSenha = null;
+      this.usuario = new Usuario(null ,null, null, null);
+  }
+
+  cadastrar(){
+    if(this.validarSenha()){
+      this.usuario.dataAceiteTermoUso = this.dataHoje();
+      this.authProvider.cadastrar(this.usuario).subscribe(
+          data => this.authSuccess(data),
+          (err: HttpErrorResponse) => {
+            this.authError(err)
+          }
+      );
+    }
+  }
+
+  validarSenha(){
+    if(this.usuario.senha.length < 6){
+      this.mostrarToast('A senha deve ter no mínimo 6 caracteres');
+    } else if (this.usuario.senha != this.confirmarSenha){
+      this.mostrarToast('As senhas digitadas são diferentes');
+    }else{
+      return true;
+    }
+    return false;
+  }
+
+  authSuccess(token) {
+    this.storage.set('token', token);
+    this.navCtrl.setRoot('ProprietarioPage');
+  }
+
+  authError(err){
+    let errorMessage : string;
+    if (err.error instanceof Error) {
+      console.log('Um erro ocorreu: ', err.error.message);
+    } else {
+        errorMessage ='Erro '+ err.status +': '+ err.error;
+    }
+    console.log(errorMessage);
+    this.mostrarToast(errorMessage);
+  }
+
+  mostrarToast(mensagem : string) {
+    let toast = this.toastCtrl.create({
+      message: mensagem,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+   dataHoje() {
+    var data = new Date();
+    var dia = data.getDate();
+    var mes = data.getMonth()+1;
+    var mesString = mes < 10 ? '0'+ mes.toString() : mes.toString();
+    var ano = data.getFullYear();
+    return [ano, mesString, dia].join('-');
+}
+
+}
