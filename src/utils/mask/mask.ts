8dc45c11f44d2d95c67a493965677f7bcd33c893
Luiz Gustavo <lgml85@hotmail.com>
2018-02-05 01:04:07 -0200

    mascara generica para demais campos

--- src/utils/mask/mask.ts
+++ src/utils/mask/mask.ts
@@ -3,6 +3,65 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Mask {
   private len: any;
+  private alphMasc: string;
+
+
+  /*
+  Informações importantes para utilizar o método "atualizarValor" para montar a mascara:
+  A = Aceitará exclusivamente letra e alterará a letra para maíscula.
+  a = Aceitará exclusivamente letra e alterará a letra para minúscula.
+  9 = Aceitará exclusivamente número.
+  x = Aceitará qualquer caracter alfanumerico.
+  *Qualquer outro caracter será entendido como separador e será aplicado assim na mascara.
+  EXEMPLO: ATZ-6369 (Máscara = AAA-9999);
+
+  */
+  atualizarValor(valor : string, mascara : string){
+    this.alphMasc = mascara.replace(/\W|_/g, '');
+    valor = valor.replace(/\W|_/g, '');
+    var i = valor.length > this.alphMasc.length ? this.alphMasc.length : valor.length;
+    var novoValor = "";
+    while(i > 0){
+    novoValor =  this.validarCaractere(valor.substring((i-1), i),this.alphMasc.substring((i-1), i))+novoValor;
+      i=i-1;
+    }
+    return this.aplicarMascara(novoValor,mascara);
+  }
+
+  private validarCaractere(carac : string, mask : string){
+    if(mask.match(/x/g)){
+      return carac;
+    }
+    if(mask.match(/[0-9]/g) && carac.match(/[0-9]/g)){
+      return carac;
+    } else if(!mask.match(/[0-9]/g) && !carac.match(/[0-9]/g)){
+      if ( mask.toUpperCase() == mask) {
+        return carac.toUpperCase();
+        }
+      return carac.toLowerCase();
+    }
+    return " ";
+  }
+  private aplicarMascara(valor : string, mascara : string){
+    var contSeparador = 0;
+    var MAX =  valor.length;
+    var novoValor = "";
+    var i = 0;
+    while(i < MAX){
+      if(!this.ehAlphaNumerico(mascara.substring((i+contSeparador), i+contSeparador+1))){
+        novoValor = novoValor+ mascara.substring((i+contSeparador), i+contSeparador+1);
+        contSeparador = contSeparador + 1;
+      }
+      novoValor = novoValor + valor.substring((i), i+1);
+      i = i + 1;
+    }
+    return novoValor;
+  }
+
+  private ehAlphaNumerico(c){
+    return !c.match(/\W|_/g);
+
+  }
 
   gerarValorMonetario(v): string {
     if (v) {
